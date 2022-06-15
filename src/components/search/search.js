8f710216184045489ba01b276428a6d3109d7bcd
Luca Bogino <luca.bogino@consulenti.csi.it>
2022-06-15 12:19:03 +0200

    add onKeyDown event in search component

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -20,13 +20,19 @@ function Search(props) {
         }
     }
 
+    const enterPress = (event) => {
+        if (event.key === "Enter") {
+            changeSelectedCity();
+        }
+    }
+
     return (
         <div className='container'>
             <Form.Label className='common-label ms-2'>Cerca</Form.Label>
             <div className='search_container common_b_radius'>
-            <input className='w-100 inp common_b_radius' value={city} placeholder="ex: Milano" onChange={(e)=>handleChangeCity(e)}></input>
-            <button className='btn_search' onClick={changeSelectedCity}>
-                <img className="w-50 mxw-50" src={searchImg}/>
+            <input className='w-100 inp common_b_radius' value={city} placeholder="ex: Milano" onChange={(e)=>handleChangeCity(e)} onKeyDown={(e)=>enterPress(e)}></input>
+            <button className='btn_search' onClick={changeSelectedCity} >
+                <img className="w-50 mxw-50" src={searchImg} alt="search"/>
             </button>
             </div>
         </div>
